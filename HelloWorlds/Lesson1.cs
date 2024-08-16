6a3a394ea213d69517bf1434cfa09ea7927e5086
Ngo Hai Long <longngohai91@gmail.com>
2024-08-16 19:54:19 +0700

    Cập nhật stopwatch

--- HelloWorlds/Lesson1.cs
+++ HelloWorlds/Lesson1.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,27 @@ namespace HelloWorlds
         [Test]
         public void Test()
         {
-            Console.WriteLine("Đây là Lesson1");
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+
+            //Code bắt đầu chạy
+
+            int soCanBo = 3;
+            double baocaoTaiChinhHopNhat = 3000;
+            string hovaTen = "Nguyen Van A";
+
+            soCanBo = (int)baocaoTaiChinhHopNhat;
+
+            Console.WriteLine(soCanBo);
+
+            //Kết thúc code
+
+            watch.Stop();
+
+            Console.WriteLine("Thời gian chạy code: " + watch.Elapsed);
+
+            
         }
 
     }
