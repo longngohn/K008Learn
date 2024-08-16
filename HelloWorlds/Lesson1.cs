170277a779c795a6b41c218d88b90642f6e8824a
Cool Cool <ngohailong1991@gmail.com>
2024-08-16 08:04:00 +0700

    Add project files.

---
+++ HelloWorlds/Lesson1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorlds
+{
+    [TestFixture] 
+    public class Lesson1
+    {
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine("Đây là Lesson1");
+        }
+
+    }
+}
