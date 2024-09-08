e965e6f4ad25ab08a4a29fb462a3bd69de0a378d
Ngo Hai Long <longngohai91@gmail.com>
2024-09-08 07:21:25 +0700

    cập nhật view

---
+++ LeanMagagement/ViewModel/GiaoViec.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeanMagagement.ViewModel
+{
+    public class GiaoViec
+    {
+        public string TenCongViec { get; set; } = "Hợp đồng GIP";
+    }
+}
