98bd92f70d47128aa4d30be321d1205b96af37f8
Ngo Hai Long <longngohai91@gmail.com>
2024-09-25 22:54:00 +0700

    update

--- LeanMagagement/App.xaml.cs
+++ LeanMagagement/App.xaml.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -14,10 +16,12 @@ namespace LeanMagagement
     /// </summary>
     public partial class App : Application
     {
+        public static CultureInfo culture = new CultureInfo("vn-VI");
         protected override void OnStartup(StartupEventArgs e)
         {
             ApplicationThemeHelper.ApplicationThemeName = Theme.Win11LightName;
             base.OnStartup(e);
+            Thread.CurrentThread.CurrentCulture = culture;
         }
     }
 }
