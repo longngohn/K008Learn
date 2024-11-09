20075e7c61a747f94c8aa3fcf67f6686f30f5cde
ngoha <ngoha@DESKTOP-ID3RNOJ>
2024-11-08 21:30:53 -0800

    up

--- LeanMagagement/Converter/TrangThaiConverter.cs
+++ LeanMagagement/Converter/TrangThaiConverter.cs
@@ -1,4 +1,5 @@
 ﻿using LeanMagagement.CLasses;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace LeanMagagement.Converter
 {
@@ -31,6 +33,84 @@ namespace LeanMagagement.Converter
                             return "Tạm dừng";
                         case TrangThai.HuyBo:
                             return "Huỷ bỏ";
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            return "Không có giá trị";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class TrangThaiColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value is TrangThai TT)
+                {
+                    switch (TT)
+                    {
+                        case TrangThai.ChuaThucHien:
+                            return Brushes.Gray;
+                        case TrangThai.DangThucHien:
+                            return Brushes.Blue;
+                        case TrangThai.DangKiemTra:
+                            return Brushes.Orange;
+                        case TrangThai.HoanThanh:
+                            return Brushes.Green;
+                        case TrangThai.TamDung:
+                            return Brushes.Red;
+                        case TrangThai.HuyBo:
+                            return Brushes.LightGray;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            return "Không có giá trị";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class TrangThaiIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value is TrangThai TT)
+                {
+                    switch (TT)
+                    {
+                        case TrangThai.ChuaThucHien:
+                            return PackIconKind.ProgressClock;
+                        case TrangThai.DangThucHien:
+                            return PackIconKind.PlayCircle;
+                        case TrangThai.DangKiemTra:
+                            return PackIconKind.DatabaseSearch;
+                        case TrangThai.HoanThanh:
+                            return PackIconKind.CheckCircle;
+                        case TrangThai.TamDung:
+                            return PackIconKind.Pause;
+                        case TrangThai.HuyBo:
+                            return PackIconKind.CloseBoxOutline;
                     }
                 }
             }
